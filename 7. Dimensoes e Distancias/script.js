611880d3f5433e29a3ab903915e6ade9592c922f
fuinho <erickalexandrex@gmail.com>
2019-06-27 00:13:53 -0300

    dimensoes e distancias

---
+++ 7. Dimensoes e Distancias/script.js
@@ -0,0 +1,9 @@
+const listaAnimais = document.querySelector('.animais-lista');
+
+// Altura do elemento
+const height = listaAnimais.scrollHeight;
+// Altura do elemento em relaçao ao topo da pagina
+const animaisTop = listaAnimais.offsetTop;
+
+console.log(listaAnimais);
+
